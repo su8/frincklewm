1bbc0c45bf0282e9792684402f117da8bc2d6af9
su8 <wifiextender2@gmail.com>
2024-08-07 01:28:32 +0300

    Update and rename frankenwm.c to frincklewm.c

--- frincklewm.c
+++ frincklewm.c
@@ -1,5 +1,20 @@
-/* see license for copyright and license */
+/*
+Copyright 08/07/2024 https://github.com/su8/frincklewm
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <err.h>
@@ -10,8 +25,10 @@
 #include <signal.h>
 #include <regex.h>
 #include <sys/wait.h>
+
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+
 #include <xcb/xcb.h>
 #include <xcb/xcb_atom.h>
 #include <xcb/xcb_aux.h>
